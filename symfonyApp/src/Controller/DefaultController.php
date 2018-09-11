a7bdff87d3eada513d5f0d67403b9927f30bddf3
Lasse Haverinen <lasse.haverinen@oamk.fi>
2018-09-11 11:52:48 +0300

    Added security bundle, added placeholder for login view

---
+++ symfonyApp/src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
